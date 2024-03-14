4198e40e12699bd1a83548928095257fe86582b8
KolosTK <tanya.kolosenko.1705@gmail.com>
2024-03-14 15:17:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-import express from 'express';
+import express from "express";
 import mongoose from 'mongoose';
-import Book from './Models/Book.js'
-import router from './router.js'
+import router from './router.js';
 
 const PORT = 6000;
+
 let app = express();
+
 app.use(express.json());
-app.use('/api',router)
+app.use('/api',router);
 
 const DB_URL ="mongodb+srv://tanyakolosenko1705:irXfH6xT8DqszQSg@cluster0.nhqth5q.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 async function StartApp(){
     try{
-        await mongoose.connect((DB_URL));
+        await mongoose.connect(DB_URL);
         app.listen (PORT, ()=>console.log('My port is working ' + PORT));
     }
     catch(error)
